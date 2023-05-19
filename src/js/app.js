43618736c952c1ac0f50b2849e952f1da7e4269b
Milana <durckina.milana2016@yandex.ru>
2023-05-20 00:07:26 +0300

    new

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+export default class ErrorRepository {
+// class ErrorRepository {
+  constructor() {
+    this.map = new Map([[123, 'Ошибка в данных'],
+      [125, 'Нет ответа от сервера'],
+    ]);
+  }
+
+  translate(code) {
+    if (this.map.has(code)) {
+      return this.map.get(code);
+    }
+    return 'Unknown error';
+  }
+}
